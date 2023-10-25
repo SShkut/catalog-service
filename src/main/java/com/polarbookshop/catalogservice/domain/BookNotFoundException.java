d0aee16c8602bf820d52615c35372b6fba0b43b7
Sviatoslav Shkut <sgshkut@gmail.com>
2023-10-25 13:18:18 +0200

    Some code

---
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookNotFoundException.java
@@ -0,0 +1,8 @@
+package com.polarbookshop.catalogservice.domain;
+
+public class BookNotFoundException extends RuntimeException {
+
+  public BookNotFoundException(String isbn) {
+    super(String.format("A book with ISBN %s already exists.", isbn));
+  }
+}
---
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookValidationTests.java
@@ -0,0 +1,36 @@
+package com.polarbookshop.catalogservice.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class BookValidationTests {
+
+  private static Validator validator;
+
+  @BeforeAll
+  static void setUp() {
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    validator = factory.getValidator();
+  }
+
+  @Test
+  void whenAllFieldsCorrectThenValidationSucceeds() {
+    var book = new Book("1234567890", "Title", "Author", 9.99);
+    var violations = validator.validate(book);
+    assertThat(violations).isEmpty();
+  }
+
+  @Test
+  void whenIsbnDefinedButIncorrectThenValidationFails() {
+    var book = new Book("1F34567890", "Title", "Author", 9.99);
+    var violations = validator.validate(book);
+    assertThat(violations).hasSize(1);
+    assertThat(violations.iterator().next().getMessage())
+        .isEqualTo("The ISBN format must be valid.");
+  }
+}
