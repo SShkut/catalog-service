219df6c1b6e1055aad7363db8324d31d476e44a8
Sviatoslav Shkut <sgshkut@gmail.com>
2023-12-17 18:05:35 +0100

    First migration

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -19,7 +19,7 @@ class CatalogServiceApplicationTests {
 
   @Test
   void whenPostRequestThenBookCreated() {
-    var expectedBook =  Book.of("1234567890", "Title", "Author", 9.99);
+    var expectedBook =  Book.of("1234567890", "Title", "Author", 9.99, "Publisher");
 
     webTestClient
         .post()
